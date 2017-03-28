6a81a30b8952346e7182b459c24b7280091ababa
George <George@Nehalem>
2017-03-29 00:53:14 +0300

    B and C subqueries almost done.

--- src/base/GitAnalyser.java
+++ src/base/GitAnalyser.java
@@ -16,9 +16,15 @@ public class GitAnalyser {
 			return;
 		
 		p = new Parse(arguments);
-		p.fileCount();
+//		p.filesCount();
+		System.out.println("Main files count: " + p.filesCount());
+//		p.totalLinesCount();
+		System.out.println("Main total lines count: " + p.totalLinesCount());
+//		p.branchCount();
+		System.out.println("Main branches count: " + p.branchCount());
+		System.out.println("Main tags count: " + p.tagsCount());
 		
-		System.out.println("Now exiting normally!");
+		System.out.println("\n\nNow exiting normally!");
 		
 	}
 
--- src/parser/Parse.java
+++ src/parser/Parse.java
@@ -2,14 +2,20 @@ package parser;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.io.BufferedInputStream;
 
 import base.ArgumentParser;
 
 import org.apache.commons.io.IOUtils;
 
 public class Parse {
+	
+	private static final boolean ALLDELIMETERS = true;
 	
 	private static final String GIT_DIR = "C:\\Program Files\\Git\\bin\\";
 	private static final String GIT_CMD = "git";
@@ -22,7 +28,7 @@ public class Parse {
 	private Process currentProcess = null;
 	private ProcessBuilder pb = null; 
 	private BufferedReader processOutpoutStream = null;
-	private String processOutput = null;
+	private String filels = null;
 	//private String[] commandArray = null;
 	
 	public Parse(ArgumentParser ap) {	
@@ -32,22 +38,104 @@ public class Parse {
 		this.outputFile = ap.outputFile;
 	}
 	
-	public int fileCount() {
-		
+	private static int countLines(String filename) throws IOException {
+	    InputStream is = new BufferedInputStream(new FileInputStream(filename));
+	    try {
+	        byte[] c = new byte[1024];
+	        int count = 0;
+	        int readChars = 0;
+	        boolean empty = true;
+	        while ((readChars = is.read(c)) != -1) {
+	            empty = false;
+	            for (int i = 0; i < readChars; ++i) {
+	            	if (ALLDELIMETERS) {
+		                if (c[i] == '\n' || c[i] == '\r') {	           
+		                    ++count;
+		                    if (c[i] == '\r' && (i+1) < readChars && c[i+1] == '\n')
+		                    	++i;
+		                }
+	            	} else {
+		                if (c[i] == '\n') {	           
+		                    ++count;
+		                }
+	            	}
+	            }
+	        }
+	        return (count == 0 && !empty) ? 1 : count;
+	    } finally {
+	        is.close();
+	    }
+	}
+	
+	public int filesCount() {
 		String args = new String("ls-files");
 		String[] commandArray = {GIT_CMD, args};
 		pb = new ProcessBuilder(commandArray);
 		pb.directory(inputFile);
 		try {
-			processOutput = IOUtils.toString(pb.start().getInputStream(), (Charset)null);
-			String[] split = processOutput.split("\r\n|\r|\n");
-			//System.out.println(processOutput);
-			System.out.println("The number of files is " + split.length);
+			filels = IOUtils.toString(pb.start().getInputStream(), (Charset)null);
+			String[] filelsSplit = filels.split("\r\n|\r|\n");
+			//System.out.println(filels);
+			System.out.println("The number of files is: " + filelsSplit.length);
+			return filelsSplit.length;
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.err.println("Could not start or get stream!");
+			System.err.println("filesCount could not start or get stream!");
+			return -1;
 		}
-		return 0;
+	}
+	
+	public int totalLinesCount() {
+		if (filels == null)
+			if (filesCount() == -1)
+				return -1;
+		String[] filelsSplit = filels.split("\r\n|\r|\n");
+		int linesSum = 0;
+		for (String singleFile : filelsSplit){
+			try {
+				linesSum += countLines(input + singleFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.err.println("TotalLinesCount could not open some file!");
+				return -1;
+			}
+		}
+		System.out.println("The total number of lines is: " + linesSum);
+		return linesSum;
+	}
+	
+	public int branchCount() {
+		String args = new String("branch");
+		String[] commandArray = {GIT_CMD, args};
+		pb = new ProcessBuilder(commandArray);
+		pb.directory(inputFile);
+		try {
+			String branches = IOUtils.toString(pb.start().getInputStream(), (Charset)null);
+			String[] branchesSplit = branches.split("\r\n|\r|\n");
+			System.out.println("The number of branches is: " + branchesSplit.length);
+			return branchesSplit.length;
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("branchCount could not start or get stream!");
+			return -1;
+		}
+	}
+	
+	public int tagsCount() {
+		String args = new String("tag");
+		String[] commandArray = {GIT_CMD, args};
+		pb = new ProcessBuilder(commandArray);
+		pb.directory(inputFile);
+		try {
+			String tags = IOUtils.toString(pb.start().getInputStream(), (Charset)null);
+			String[] tagsSplit = tags.split("\r\n|\r|\n");
+			System.out.println("The number of tags is: " + tagsSplit.length);
+			return tagsSplit.length;
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("tagsCount could not start or get stream!");
+			return -1;
+		}
 	}
 	
 }
